e59639f97d42b49fe9d404722d25e1c656bac033
Eugeniy Stasuk <estasuk@markupus.tech>
2024-08-30 10:55:03 +0300

    themes updated

---
+++ wp-content/themes/uhc/footer.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Footer default template.
+ *
+ * @see Options -> Footer.
+ *
+ * @package WordPress
+ * @subpackage UHC
+ */
+?>
+
+			<footer class="footer">
+			</footer>
+			<?php wp_footer() ?>
+		</div><!-- .wrapper -->
+    </body>
+</html>
+
