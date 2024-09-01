422906dfcd5617165a4866670577272822463e25
Eugeniy Stasuk <estasuk@markupus.tech>
2024-09-01 17:46:24 +0300

    button styles

--- wp-content/themes/uhc/theme-functions/theme-functions.php
+++ wp-content/themes/uhc/theme-functions/theme-functions.php
@@ -7,6 +7,12 @@ function get_theme_link($btnArr = false, $class = '')
     $target = !empty($btnArr['target']) ? 'target="' . $btnArr['target'] . '"' : '';
     $class = !empty($class) ? 'class="' . $class . '"' : '';
 
+    if(str_contains($class, 'button') || str_contains($class, 'btn')){
+        $icon = get_template_directory() . '/src/images/btn-arrow.svg';
+        $title = '<span>' . $title . '</span>';
+        $title .= file_exists($icon) ? '<span>' . file_get_contents($icon) . '</span>' : '';
+    }
+
     return sprintf('<a %s %s %s>%s</a>', $class, $href, $target, $title);
 }
 
