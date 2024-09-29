1986397d1cb77d45c0111bd0b3fb843d76099fd2
Eugeniy Stasuk <estasuk@markupus.tech>
2024-09-29 12:14:27 +0300

    seo plugin updated

--- wp-content/themes/uhc/functions.php
+++ wp-content/themes/uhc/functions.php
@@ -77,3 +77,9 @@ if( function_exists( 'acf_add_options_page' ) ){
 	);
 }
 
+function my_acf_init() {
+    acf_update_setting('google_api_key', 'AIzaSyCfjg5wFo7Lv98-2fPXuTHyLYsrxSrHRF4');
+}
+
+add_action('acf/init', 'my_acf_init');
+
