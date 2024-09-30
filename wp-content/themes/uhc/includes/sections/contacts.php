f4bed00572723dce9d23e06e87523dae09b8db96
Eugeniy Stasuk <estasuk@markupus.tech>
2024-09-30 22:58:40 +0300

    contact page desctop

--- wp-content/themes/uhc/includes/sections/contacts.php
+++ wp-content/themes/uhc/includes/sections/contacts.php
@@ -3,6 +3,8 @@ $text = get_sub_field('text');
 $social_links_label = get_sub_field('social_links_label');
 $address_map = get_field('address_map', 'option');
 $address_text = get_field('address_text', 'option');
+$email = get_field('e-mail', 'option');
+$social = get_field('social', 'option');
 ?>
 
 <section class="contacts">
@@ -25,6 +27,45 @@ $address_text = get_field('address_text', 'option');
 
         <div class="contacts-content">
             <?php echo $text ?>
+        </div>
+        
+        <div class="contacts-social">
+            <?php
+            if(!empty($social_links_label)){
+                echo '<div class="contacts-social__title"><span>' . $social_links_label . '</span></div>';
+            }
+            ?>
+
+            <?php
+            if(!empty($email)):
+                $icon = get_template_directory() . '/src/images/email2.svg';
+                $icon = file_exists($icon) ? '<span>' . file_get_contents($icon) . '</span>' : '';
+
+                echo get_theme_link(
+                        [
+                            'url' => 'mailto:' . $email,
+                            'title' => $icon . '<span>' . $email . '</span>',
+                            'target' => '_blank',
+                        ],
+                        'contacts-social__link',
+                );
+            endif;
+            ?>
+
+            <?php
+            if(!empty($social)):
+                foreach($social as $item):
+                    $icon = !empty($item['icon']) ? wp_get_original_image_path($item['icon']) : '';
+                    $icon = file_exists($icon) ? '<span>' . file_get_contents($icon) . '</span>' : '';
+                    $text = !empty($item['link']['title']) ? $item['link']['title'] : '';
+                    $item['link']['title'] = $icon . '<span>' . $text . '</span>';
+
+                    echo get_theme_link(
+                        $item['link'],
+                        'contacts-social__link',
+                    );
+                endforeach;
+            endif; ?>
         </div>
     </div>
 </section>
